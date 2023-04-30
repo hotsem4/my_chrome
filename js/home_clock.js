fefa0f659acdc45a1acce716c4e329d4c7902856
bumsu Choe <hotsem100@gmail.com>
2023-05-01 05:34:52 +0900

    링크만 수정하면 되는데...

--- js/home_clock.js
+++ js/home_clock.js
@@ -1,33 +1,3 @@
-// 'use strict';
-
-// function setClock() {
-//   let dateInfo = new Date();
-//   let hour = modifyNumber(dateInfo.getHours());
-//   let min = modifyNumber(dateInfo.getMinutes());
-//   let sec = modifyNumber(dateInfo.getSeconds());
-//   let year = dateInfo.getFullYear();
-//   let month = dateInfo.getMonth() + 1; //monthIndex를 반환해주기 때문에 1을 더해준다.
-//   let date = dateInfo.getDate();
-//   let dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'][dateInfo.getDay()];
-//   document.getElementById('time').innerHTML = hour + ':' + min;
-//   document.getElementById(
-//     'date'
-//   ).innerHTML = `${month}월 ${date}일 (${dayOfWeek})`;
-//   document.getElementById(
-//     'mainDate'
-//   ).innerHTML = `${year}년 ${month}월 ${date}일 (${dayOfWeek})`;
-//   document.getElementById('mainTime').innerHTML = `${hour}:${min}:${sec}`;
-// }
-// function modifyNumber(time) {
-//   if (parseInt(time) < 10) {
-//     return '0' + time;
-//   } else return time;
-// }
-// window.onload = function () {
-//   setClock();
-//   setInterval(setClock, 1000); //1초마다 setClock 함수 실행
-// };
-
 'use strict';
 
 function setClock() {
---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const weather = document.querySelector('#weather span:first-child');
+const city = document.querySelector('#weather span:last-child');
+const API_KEY = 'e067c23109efc86fc64a24740ecc1c55';
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
